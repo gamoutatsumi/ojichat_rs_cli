46ac8e6d49d32ae2e7e2f68949b888a3189a175a
gamoutatsumi <47162587+gamoutatsumi@users.noreply.github.com>
2021-01-11 16:20:11 +0900

    del Dockerfile

--- Dockerfile
+++
@@ -1,5 +0,0 @@
-FROM ekidd/rust-musl-builder:latest
-
-ADD --chown=rust:rust . ./
-
-CMD cargo build --release
--- entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-
-sudo chown -R rust:rust /home/rust/src
-
-cargo build --release --verbose
-
-sudo chown -R 1001:1001 /home/rust/src
